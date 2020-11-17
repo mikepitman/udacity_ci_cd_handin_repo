58f16b3e443486741f1e1dd274d2dd565d837a98
Michael Pitman <mikepitman@users.noreply.github.com>
2020-11-17 13:56:56 +0200

    Update make_predict_azure_app.sh
    Update URL with app name

--- flask-sklearn/make_predict_azure_app.sh
+++ flask-sklearn/make_predict_azure_app.sh
@@ -25,5 +25,6 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+     #-X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+     -X POST https://udacity-mike-cicd-handin.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
