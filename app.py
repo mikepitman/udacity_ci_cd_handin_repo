b061aaa8bafe1ff8bb5d0b750940670520bb39a5
mike <mike@ubiquitech.co.za>
2020-11-20 12:21:17 +0200

    Checking triggering of automation

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home - with pipelines! Different pipeline now</h3>"
+    html = "<h3>Sklearn Prediction Home - with pipelines! Does the pipeline pick up changes automatically?</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
