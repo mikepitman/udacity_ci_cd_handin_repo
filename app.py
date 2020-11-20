474df1d0976141577b4063cfefbaa15826c3080c
mike <mike@ubiquitech.co.za>
2020-11-20 13:05:05 +0200

    Changed message to validate automatic pipelining again

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home - with pipelines! Yaml trigger changed to 'main' from 'master'</h3>"
+    html = "<h3>Sklearn Prediction Home - Pipelines working correctly now.</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
