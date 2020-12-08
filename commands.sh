b6f482c129b368220f87fd9cb5860516d640730c
mike <mike@ubiquitech.co.za>
2020-12-08 16:57:39 +0200

    Last try for locust

--- commands.sh
+++ commands.sh
@@ -12,5 +12,6 @@ chmod 744 make_predict*
 # Edit the script, set the URL to your app name for HTTP POST
 vi make_predict_azure_app.sh
 ./make_predict_azure_app.sh
+pip install locust
 # Edit the command, set the URL to your app name for --host
 locust -f locust.py --headless --host https://udacity-mike-cicd-handin.azurewebsites.net --users 10 --run-time 60s --spawn-rate 2
